89c56e3c0b6e57ee2f66dcbd36ffe4e8cfb6fe0e
Paolo Flores <pflores.fisi22@gmail.com>
2024-09-24 15:41:14 -0500

    add 🧵 project1/task1 notes and format code

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -89,9 +89,8 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
-
-    /* The tick to wake up the thread */
-    int64_t wakeup_tick;
+    int64_t wakeup_tick;                /* 🧵 project1/task1
+                                           The tick to wake up the thread */
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
@@ -122,7 +121,8 @@ tid_t thread_create (const char *name, int priority, thread_func *, void *);
 void thread_block (void);
 void thread_unblock (struct thread *);
 
-/* New implemented functions */
+// 🧵 project1/task1 definitions
+
 void thread_sleep (int64_t ticks);
 void thread_awake(int64_t ticks);
 bool thread_wakeup_tick_less (const struct list_elem *a_, const struct list_elem *b_, void *aux UNUSED);
